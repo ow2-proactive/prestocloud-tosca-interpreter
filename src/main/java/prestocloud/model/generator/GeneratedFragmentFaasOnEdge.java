760c8db9408a5f1be00df7986a9d96256b6e04de
Maxime COMPASTIÉ <maxime.compastie@activeeon.com>
2019-12-24 01:27:59 +0100

    Generating edge fragment model (except. capabilities)

--- src/main/java/prestocloud/model/generator/GeneratedFragmentFaasOnEdge.java
+++ src/main/java/prestocloud/model/generator/GeneratedFragmentFaasOnEdge.java
@@ -24,8 +24,43 @@ public class GeneratedFragmentFaasOnEdge extends GeneratedNode {
     // Get the info structured
 
     public String getStructureProcessingNode() {
-        // TODO
-        return "";
+        StringBuilder sb = new StringBuilder();
+        String headerUnstructured = "   processing_node_%s:\n" +
+                "      type: prestocloud.nodes.compute.edge\n" +
+                "      properties:\n" +
+                "         id: %s\n" +
+                "         type: edge\n";
+        String headerNameunstructure = "         name: %s\n";
+        String edgeUnstructured = "         edge:\n" +
+                "            edge_type: %s\n" +
+                "            edge_location: %s\n" +
+                "            edge_credentials:\n" +
+                "               username: %s\n";
+        String edgePasswordUnstructured = "               password: %s\n";
+        String edgePrivateKeyUnstructured = "               privatekey: %s\n";
+        String edgeGpsCoordinatesUnstructured = "            gps_coordinates: %s\n";
+        String networkUnstructured = "         network:\n" +
+                "         network_name: %s\n" +
+                "         network_id: %s\n" +
+                "         addresses:" +
+                "            - @variables_network_%s\n";
+        String capabilitiesUnstructured = "      capabilities:\n";
+
+        sb.append(String.format(headerUnstructured, this.fragmentName, this.computeId));
+        if (this.computeName.isPresent()) {
+            sb.append(String.format(headerNameunstructure, computeName.get()));
+        }
+        sb.append(String.format(edgeUnstructured, this.edgeType, this.edgeLocation, edgeCredentialsUsername));
+        if (edgeCredentialsPassword.isPresent()) {
+            sb.append(String.format(edgePasswordUnstructured, edgeCredentialsPassword.get()));
+        }
+        if (edgeCredentialsPrivateKey.isPresent()) {
+            sb.append(String.format(edgePrivateKeyUnstructured, edgeCredentialsPrivateKey.get()));
+        }
+        if (edgeGpsCoordinate.isPresent()) {
+            sb.append(String.format(edgeGpsCoordinatesUnstructured, edgeGpsCoordinate.get()));
+        }
+        return sb.toString();
     }
 
 }
